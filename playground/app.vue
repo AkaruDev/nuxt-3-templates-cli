065da0723ac620f23533314076dd207e66122064
Adrien Feletti <gingifruden@gmail.com>
2023-05-05 10:04:40 +0200

    Playground for test purpose

---
+++ playground/app.vue
@@ -0,0 +1,35 @@
+<template>
+  <main>
+    <h1>Nuxt 3 templates CLI</h1>
+    <div>Playground</div>
+  </main>
+</template>
+
+
+<style >
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+main {
+  position: relative;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100vh;
+
+  gap: 10px;
+
+  background-color: #1f1f1f;
+  color: white;
+}
+
+h1 {
+  font-size: 54px;
+}
+</style>
---
+++ playground/modules/virtual-scroll/playground/app.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-scroll-container>
+    <VirtualScrollBar />
+    <div v-scroll-section>
+      <NuxtPage />
+    </div>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.Page {
+  position: relative;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+
+  width: calc(100% - 30px);
+  min-height: 100vh;
+
+  gap: 10px;
+
+  text-align: center;
+
+  overflow: hidden;
+
+  font-size: 16px;
+}
+
+h1 {
+  font-size: 58px;
+
+  text-transform: uppercase;
+}
+</style>
